4be108b9e4a84b5fb48be5d9e33b388fb229bb32
sidney <ojsidney044@gmail.com>
2025-01-16 15:29:04 +0300

    Hero's section done

---
+++ components/Helper/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from "./Home/Hero/Hero"
+import About from './Home/About/About'
+
+const Home = () => {
+  return (
+    <div className="">
+        <Hero/>
+        <About/>
+        </div>
+  )
+}
+
+export default Home
