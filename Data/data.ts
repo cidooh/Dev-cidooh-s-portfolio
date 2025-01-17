705b79adc69294c270add6793f6be4f07fe0bdb8
sidney <ojsidney044@gmail.com>
2025-01-17 14:53:16 +0300

    debugging

--- Data/data.ts
+++ Data/data.ts
@@ -22,43 +22,43 @@ export const skillsData = [
     {
         id: 1,
         title:"React",
-        image:"/public/image/React.png",
+       
         percent:"80%",
     },
     {
         id:2,
         title:"CSS",
-        image:"/public/image/React.png",
+       
         percent:"95%"
     },
     {
         id:3,
         title:"HTML",
-        image:"/public/image/React.png",
+        
         percent:"70%"
     },
     {
         id:4,
         title:"Python",
-        image:"/public/image/Html.png",
+        
         percent:"36%%"
     },
     {
         id:5,
         title:"Django",
-        image:"/public/image/React.png",
+        
         percent:"30%"
     },
     {
         id:6,
         title:"Firebase",
-        image:"/public/image/React.png",
+        
         percent:"60%"
     },
     {
         id:7,
         title:"Figma",
-        image:"/public/image/React.png",
+        
         percent:"70%"
     },    
      
