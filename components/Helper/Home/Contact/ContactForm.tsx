022975ac920dfc6784d19f724972afaf762eb59d
sidney <ojsidney044@gmail.com>
2025-01-17 14:38:27 +0300

    made fewchanges

--- components/Helper/Home/Contact/ContactForm.tsx
+++ components/Helper/Home/Contact/ContactForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useRef, useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const form = useRef(null);
